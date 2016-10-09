8be4a9ad7f33ebf8fa871660545d00196c22b99e
Michael Fulton <fultonms@clarkson.edu>
2016-10-09 00:28:35 +0000

    Redo with alexa-app

---
+++ src/app.js
@@ -0,0 +1,15 @@
+var alexa = require('alexa-app');
+var app = new alexa.app('astronomer');
+
+app.launch(function(request, response){
+  response.say("I can tell you when the sun will rise or set.");
+});
+
+app.intent('getsunrise',
+  function(request, response){
+    sunriseMessage = "Nooooooooon";
+    response.say(sunriseMessage");
+  }
+);
+
+exports.handler = app.lambda();
