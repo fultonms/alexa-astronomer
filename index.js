5e7e0504e6872902572a742ad829461c51094018
Michael Fulton <fultonms@clarkson.edu>
2016-10-08 21:51:38 -0400

    Added Backends for Sunrise and Sunset

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+require("request");
+
+var sunbaseURL = "http://api.sunrise-sunset.org/json?";
+var lat = 43.048122;
+var long = -76.147424;
+
 var alexa = require('alexa-app');
 var app = new alexa.app('astronomer');
 
@@ -5,17 +11,37 @@ app.launch(function(request, response){
   response.say("I can tell you when the sun will rise or set.");
 });
 
-app.intent('GetSunriseIntent',
+app.intent('GetSunrise',
   function(request, response){
-    sunriseMessage = "Nooooooooon";
+    var url = sunbaseURL + "lat=" + lat + "&lng=" + long;
+    var sunriseMessage = "";
+    request({
+      url: url,
+      json: true
+    }, function(error, response, body) {
+        if(!error && response.statusCode === 200){
+          obj = JSON.parse(body);
+          sunriseMessage = obj.sunrise;
+        }
+    });
     response.say(sunriseMessage);
   }
 );
 
-app.intent('GetSunsetIntent',
+app.intent('GetSunset',
   function(request, response){
-    sunriseMessage = "None";
-    response.say(sunriseMessage);
+    var url = sunbaseURL + "lat=" + lat + "&lng=" + long;
+    var sunseteMessage = "";
+    request({
+      url: url,
+      json: true
+    }, function(error, response, body) {
+        if(!error && response.statusCode === 200){
+          obj = JSON.parse(body);
+          sunriseMessage = obj.sunset;
+        }
+    });
+    response.say(sunsetMessage);
   }
 );
 
